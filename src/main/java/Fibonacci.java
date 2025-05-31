b4e13678b989330e9fcfe6be27b0c06c76077b30
Neha Parween <nparween+cisco@cisco.com>
2025-05-31 15:21:52 +0530

    Prime Number and Fibonnaci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -25,4 +25,5 @@ public class Fibonacci {
     public static void main(String[] args) {
         generateFibonacciSeries(10); // Prints the first 10 terms of the Fibonacci series
     }
+
 }
--- src/main/java/PrimeNumber.java
+++ src/main/java/PrimeNumber.java
@@ -21,6 +21,7 @@ public class PrimeNumber {
             return;
         }
 
+
         // Create a boolean array to mark prime numbers
         boolean[] isPrime = new boolean[limit + 1];
         Arrays.fill(isPrime, true); // Assume all numbers are prime initially
